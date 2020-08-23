6caedc50928d03a06ccf71c844f3cf49b0b26089
AlmAtoi <atoimbekov@gmail.com>
2020-08-23 07:48:09 +0600

    cordova: initial settings

--- client/vue.config.js
+++ client/vue.config.js
@@ -2,12 +2,21 @@ const config = {
   "transpileDependencies": [
     "vuetify"
   ],
-  publicPath: '/',
+
+  publicPath: '',
+
+  pluginOptions: {
+    cordovaPath: 'cordova'
+  }
 }
 if (process.env.NODE_ENV === 'development') {
   config.devServer = {
     proxy: `http://localhost:${process.env.PORT}`
   }
+} else {
+    config.devServer = {
+      proxy: 'https://aida-aps.herokuapp.com'
+    }
 }
 
 module.exports = config
